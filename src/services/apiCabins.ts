469f85ebf434f4ec943115630f7985daaa07722a
Gabriel Low <lowgabriel213@gmail.com>
2024-09-23 20:12:55 +0800

    refactor: update getCabins to return Promise<Cabin[]>

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -1,12 +1,25 @@
+import { Cabin } from "../types/cabin";
 import supabase from "./supabase";
 
-export async function getCabins() {
+export async function getCabins(): Promise<Cabin[]> {
   const { data: cabins, error } = await supabase.from("cabins").select("*");
 
   if (error) {
     console.error("Error fetching cabins:", error);
-    return null;
+    return [];
   }
 
   return cabins;
+}
+
+export async function deleteCabin(id: number) {
+  const { error } = await supabase
+    .from("cabins")
+    .delete()
+    .eq("some_column", id);
+
+  if (error) {
+    console.error("Error deleting cabin:", error);
+    return null;
+  }
 }
