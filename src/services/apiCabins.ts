99a5acc49d4193e2b79d09cce42f880b8b0c65ac
Gabriel Low <lowgabriel213@gmail.com>
2024-09-30 16:36:43 +0800

    feat: add duplication for create

--- src/services/apiCabins.ts
+++ src/services/apiCabins.ts
@@ -13,13 +13,26 @@ export async function getCabins(): Promise<Cabin[]> {
 }
 
 export async function createCabin(newCabin: Cabin): Promise<Cabin[]> {
-  const imageName = `${Math.random()}-${newCabin.image[0].name}`.replace(
-    /\//g,
-    ""
-  );
-  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+  let imagePath = "";
 
-  // 1. Create cabin
+  if (newCabin.image instanceof FileList) {
+    const imageName = `${Math.random()}-${newCabin.image[0].name}`.replace(
+      /\//g,
+      ""
+    );
+    imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+    const { error: storageError } = await supabase.storage
+      .from("cabin-images")
+      .upload(imageName, newCabin.image[0]);
+    if (storageError) {
+      console.error(storageError);
+      throw new Error("New image could not be uploaded");
+    }
+  } else {
+    // no new image uploaded
+    imagePath = newCabin.image;
+  }
+
   const { data, error: createError } = await supabase
     .from("cabins")
     .insert([{ ...newCabin, image: imagePath }])
@@ -29,26 +42,14 @@ export async function createCabin(newCabin: Cabin): Promise<Cabin[]> {
     throw new Error("Cabin could not be created");
   }
 
-  // 2. Upload image
-  const { error: storageError } = await supabase.storage
-    .from("cabin-images")
-    .upload(imageName, newCabin.image[0]);
-  // Delete cabin if the image was not uploaded successfully
-  if (storageError) {
-    await supabase.from("cabins").delete().eq("id", data[0].id);
-    console.error(storageError);
-    throw new Error("Cabin image could not be uploaded");
-  }
-
   return data;
 }
 
 export async function editCabin(newCabin: Cabin, id: number): Promise<Cabin[]> {
-  let newImageName = "";
   let newImagePath = "";
 
   if (newCabin.image instanceof FileList) {
-    newImageName = `${Math.random()}-${newCabin.image[0].name}`.replace(
+    const newImageName = `${Math.random()}-${newCabin.image[0].name}`.replace(
       /\//g,
       ""
     );
