6b58188254ba919bc437767bd6e464dbe751411b
Gabriel Low <lowgabriel213@gmail.com>
2024-09-30 15:31:00 +0800

    refactor: convert fetch cabins query into hook

--- src/features/cabins/CabinTable.tsx
+++ src/features/cabins/CabinTable.tsx
@@ -1,6 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useCabins } from "./useCabins";
+
 import styled from "styled-components";
-import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 
@@ -29,10 +29,7 @@ const TableHeader = styled.header`
 `;
 
 export default function CabinTable() {
-  const { isLoading, data: cabins } = useQuery({
-    queryKey: ["cabins"],
-    queryFn: getCabins,
-  });
+  const { cabins, isLoading } = useCabins();
 
   if (isLoading) return <Spinner />;
 
