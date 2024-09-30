5e3f2f2a0bb9cd6c0c1a968c920de6724fbab324
Gabriel Low <lowgabriel213@gmail.com>
2024-09-30 17:00:23 +0800

    feat: add view settings page

---
+++ src/features/settings/UpdateSettingsForm.tsx
@@ -0,0 +1,52 @@
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import Spinner from "../../ui/Spinner";
+import { useSettings } from "./useSettings";
+
+export default function UpdateSettingsForm() {
+  const {
+    settings: {
+      min_booking_length,
+      max_booking_length,
+      max_guests_per_booking,
+      breakfast_price,
+    } = {},
+    isLoading,
+  } = useSettings();
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <Form>
+      <FormRow label="Minimum nights/booking">
+        <Input
+          type="number"
+          id="min-nights"
+          defaultValue={min_booking_length}
+        />
+      </FormRow>
+      <FormRow label="Maximum nights/booking">
+        <Input
+          type="number"
+          id="max-nights"
+          defaultValue={max_booking_length}
+        />
+      </FormRow>
+      <FormRow label="Maximum guests/booking">
+        <Input
+          type="number"
+          id="max-guests"
+          defaultValue={max_guests_per_booking}
+        />
+      </FormRow>
+      <FormRow label="Breakfast price">
+        <Input
+          type="number"
+          id="breakfast-price"
+          defaultValue={breakfast_price}
+        />
+      </FormRow>
+    </Form>
+  );
+}
