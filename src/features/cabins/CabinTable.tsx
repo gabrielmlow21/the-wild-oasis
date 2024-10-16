6a88b82a16cedb07c884b1e7e5f38f1230491e65
Gabriel Low <lowgabriel213@gmail.com>
2024-10-16 21:01:01 +0800

    feat: add discount filter

--- src/features/cabins/CabinTable.tsx
+++ src/features/cabins/CabinTable.tsx
@@ -1,5 +1,5 @@
 import { useCabins } from "./useCabins";
-
+import { useSearchParams } from "react-router-dom";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
@@ -8,8 +8,19 @@ import Menus from "../../ui/Menus";
 
 export default function CabinTable() {
   const { cabins, isLoading } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins: Cabin[] | undefined = [];
+
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
 
   return (
     <Menus>
@@ -23,7 +34,7 @@ export default function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin: Cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
